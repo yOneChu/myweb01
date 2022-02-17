2904242168eda078be537ba4639d17345911a79e
kyhslam <kyhslam@gmail.com>
2022-02-18 00:30:52 +0900

    validator

--- src/main/java/com/kyhslam/myweb/controller/BoardController.java
+++ src/main/java/com/kyhslam/myweb/controller/BoardController.java
@@ -2,12 +2,14 @@ package com.kyhslam.myweb.controller;
 
 import com.kyhslam.myweb.model.Board;
 import com.kyhslam.myweb.repository.BoardRepository;
+import com.kyhslam.myweb.validator.BoardValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -17,11 +19,40 @@ public class BoardController {
 
     private final BoardRepository boardRepository;
 
+    private final BoardValidator boardValidator;
+
     @GetMapping("/list")
     public String list(Model model) {
-
+        System.out.println("list ## ");
         List<Board> boards = boardRepository.findAll();
         model.addAttribute("boards", boards);
         return "board/list";
+    }
+
+    @GetMapping("/form")
+    public String form(Model model, @RequestParam(required = false) Long id) {
+        System.out.println("id = " + id);
+        if (id == null) {
+            model.addAttribute("board", new Board());
+        } else {
+            Board board = boardRepository.findById(id).orElse(null);
+            model.addAttribute("board", board);
+        }
+
+        return "board/form";
+    }
+
+    @PostMapping("/form")
+    public String greetingSubmit(@Valid Board board, BindingResult bindingResult) {
+
+        boardValidator.validate(board, bindingResult);
+
+        if (bindingResult.hasErrors()) {
+            return "board/form";
+        }
+        System.out.println("greetingSubmit = " + board);
+
+        boardRepository.save(board);
+        return "redirect:/board/list";
     }
 }
--- src/main/java/com/kyhslam/myweb/model/Board.java
+++ src/main/java/com/kyhslam/myweb/model/Board.java
@@ -6,6 +6,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Data
@@ -14,7 +16,8 @@ public class Board {
     @GeneratedValue(strategy = GenerationType.AUTO) // 기본값 : AUTO
     private long id;
 
-
+    @NotNull
+    @Size(min = 2, max = 30, message = "제목은 2자리 이상 30자 이하 이다.")
     private String title;
     private String content;
 
